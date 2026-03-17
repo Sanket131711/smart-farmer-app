7202740f9bb7440e59305bdab4dfc9569f0b016e
Sanket131711 <shindesankett1317@gmail.com>
2026-03-17 07:33:57 +0530

    Smart farmer project files

---
+++ backend.py
@@ -0,0 +1,100 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask import send_from_directory
+import random
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return send_from_directory(".", "farmer.html")
+# Crop data
+crops = {
+    "cotton": {"pesticide":"Imidacloprid","dose":200,"water":150},
+    "wheat": {"pesticide":"Chlorpyrifos","dose":180,"water":140},
+    "rice": {"pesticide":"Cartap Hydrochloride","dose":220,"water":160},
+    "onion": {"pesticide":"Mancozeb","dose":150,"water":120},
+    "soybean": {"pesticide":"Lambda-cyhalothrin","dose":170,"water":130},
+    "sugarcane": {"pesticide":"Thiamethoxam","dose":250,"water":200}
+}
+
+# Get crop data
+@app.route("/crop/<name>")
+def crop(name):
+    if name in crops:
+        return jsonify(crops[name])
+    return jsonify({"error":"Crop not found"})
+
+
+# Spray calculation
+@app.route("/calculate", methods=["POST"])
+def calculate():
+
+    data=request.json
+
+    area=float(data["area"])
+    dose=float(data["dose"])
+    water=float(data["water"])
+    tank=float(data["tank"])
+
+    totalMed=area*dose
+    totalWater=area*water
+
+    tankCount=totalWater/tank
+    perTank=totalMed/tankCount
+
+    return jsonify({
+
+        "medicine":round(totalMed,2),
+        "water":round(totalWater,2),
+        "tanks":round(tankCount,1),
+        "perTank":round(perTank,2)
+
+    })
+
+
+# Weather
+@app.route("/weather")
+def weather():
+
+    rain=random.choice([True,False])
+
+    if rain:
+
+        return jsonify({
+            "weather":"Rain expected",
+            "advice":"Avoid spraying today"
+        })
+
+    return jsonify({
+        "weather":"Clear weather",
+        "advice":"Good for spraying"
+    })
+
+
+# Disease detect
+@app.route("/disease")
+def disease():
+
+    diseases=[
+        "Leaf Spot",
+        "Powdery Mildew",
+        "Rust Disease",
+        "Bacterial Blight",
+        "Healthy Leaf"
+    ]
+
+    result=random.choice(diseases)
+
+    return jsonify({
+
+        "disease":result,
+        "solution":"Neem Oil Spray"
+
+    })
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
