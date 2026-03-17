79d1ac1a8ef4289c71a846ccb291079766a4ddb9
Sanket131711 <shindesankett1317@gmail.com>
2026-03-17 08:31:16 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -97,4 +97,8 @@ def disease():
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
